2a6044540c55422e312d7ff674e3db8e941ef18d
André Pestana Silva <atlas@pop-os.localdomain>
2023-05-04 01:48:57 -0300

    feat: cria rotas para listagem de Car

--- src/Routes/car.routes.ts
+++ src/Routes/car.routes.ts
@@ -3,6 +3,16 @@ import CarController from '../Controllers/CarController';
 
 const carRoutes = Router();
 
+carRoutes.get(
+  '/',
+  (req, res, next) => new CarController(req, res, next).findAll(),
+);
+
+carRoutes.get(
+  '/:id',
+  (req, res, next) => new CarController(req, res, next).findById(),
+);
+
 carRoutes.post(
   '/',
   (req, res, next) => new CarController(req, res, next).create(),
