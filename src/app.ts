a40ea6acbf58f247eb15bff65f9c194444bee9b0
André Pestana Silva <atlas@pop-os.localdomain>
2023-05-04 01:47:45 -0300

    feat: configura o middleware de erro no app

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import carRoutes from './Routes/car.routes';
+import httpErrorMiddleware from './Middlewares/errorMiddleware';
 
 const app = express();
 app.use(express.json());
 app.use('/cars', carRoutes);
 
+app.use(httpErrorMiddleware);
 export default app;
